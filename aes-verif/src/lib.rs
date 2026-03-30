88610d6447f5e0fa13263567e52daece7939cd40
Bretton <bretton@galois.com>
2026-03-29 17:32:28 -0700

    AES: Rename in_block to block in encrypt128

--- aes-verif/src/lib.rs
+++ aes-verif/src/lib.rs
@@ -3,10 +3,10 @@ use aes::{
     cipher::{BlockEncrypt, KeyInit, generic_array::GenericArray},
 };
 
-pub fn encrypt128(key: [u8; 16], in_block: [u8; 16]) -> [u8; 16] {
+pub fn encrypt128(key: [u8; 16], block: [u8; 16]) -> [u8; 16] {
     let key = GenericArray::from(key);
-    let mut in_block = GenericArray::from(in_block);
+    let mut block = GenericArray::from(block);
     let cipher = Aes128::new(&key);
-    cipher.encrypt_block(&mut in_block);
-    in_block.into()
+    cipher.encrypt_block(&mut block);
+    block.into()
 }
