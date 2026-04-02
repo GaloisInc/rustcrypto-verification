3f1a7c0274d6271a350297f158c4cca06732efe4
Bretton <bretton@galois.com>
2026-04-02 01:25:27 -0700

    AES: Verify AES-128 decryption

--- aes-verif/src/lib.rs
+++ aes-verif/src/lib.rs
@@ -1,6 +1,8 @@
 use aes::{
     Aes128,
-    cipher::{BlockEncrypt, KeyInit, generic_array::GenericArray},
+    cipher::{
+        BlockDecrypt, BlockEncrypt, KeyInit, generic_array::GenericArray,
+    },
 };
 
 pub fn encrypt128(key: [u8; 16], block: [u8; 16]) -> [u8; 16] {
@@ -8,5 +10,13 @@ pub fn encrypt128(key: [u8; 16], block: [u8; 16]) -> [u8; 16] {
     let mut block = GenericArray::from(block);
     let cipher = Aes128::new(&key);
     cipher.encrypt_block(&mut block);
+    block.into()
+}
+
+pub fn decrypt128(key: [u8; 16], block: [u8; 16]) -> [u8; 16] {
+    let key = GenericArray::from(key);
+    let mut block = GenericArray::from(block);
+    let cipher = Aes128::new(&key);
+    cipher.decrypt_block(&mut block);
     block.into()
 }
