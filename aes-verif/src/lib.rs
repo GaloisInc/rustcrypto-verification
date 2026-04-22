68d56104eac1163ffc74d15cedc60be8b51f26db
Bretton <bretton@galois.com>
2026-04-22 11:43:16 -0700

    AES: Verify AES-192 and 256 encrypt and decrypt

--- aes-verif/src/lib.rs
+++ aes-verif/src/lib.rs
@@ -1,5 +1,5 @@
 use aes::{
-    Aes128,
+    Aes128, Aes192, Aes256,
     cipher::{
         BlockDecrypt, BlockEncrypt, KeyInit, generic_array::GenericArray,
     },
@@ -17,6 +17,38 @@ pub fn decrypt128(key: [u8; 16], block: [u8; 16]) -> [u8; 16] {
     let key = GenericArray::from(key);
     let mut block = GenericArray::from(block);
     let cipher = Aes128::new(&key);
+    cipher.decrypt_block(&mut block);
+    block.into()
+}
+
+pub fn encrypt192(key: [u8; 24], block: [u8; 16]) -> [u8; 16] {
+    let key = GenericArray::from(key);
+    let mut block = GenericArray::from(block);
+    let cipher = Aes192::new(&key);
+    cipher.encrypt_block(&mut block);
+    block.into()
+}
+
+pub fn decrypt192(key: [u8; 24], block: [u8; 16]) -> [u8; 16] {
+    let key = GenericArray::from(key);
+    let mut block = GenericArray::from(block);
+    let cipher = Aes192::new(&key);
+    cipher.decrypt_block(&mut block);
+    block.into()
+}
+
+pub fn encrypt256(key: [u8; 32], block: [u8; 16]) -> [u8; 16] {
+    let key = GenericArray::from(key);
+    let mut block = GenericArray::from(block);
+    let cipher = Aes256::new(&key);
+    cipher.encrypt_block(&mut block);
+    block.into()
+}
+
+pub fn decrypt256(key: [u8; 32], block: [u8; 16]) -> [u8; 16] {
+    let key = GenericArray::from(key);
+    let mut block = GenericArray::from(block);
+    let cipher = Aes256::new(&key);
     cipher.decrypt_block(&mut block);
     block.into()
 }
