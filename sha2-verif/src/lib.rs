37697a6abbbfc767f2bb7b01188a98f6d45c25d5
Bretton <bretton@galois.com>
2026-05-16 06:57:41 -0700

    sha2: Verify update, finalize, digest

--- sha2-verif/src/lib.rs
+++ sha2-verif/src/lib.rs
@@ -1,12 +1,17 @@
 use sha2::*;
 
+const HELLO: &[u8; 11] = b"hello world";
+const LONG_MESSAGE: &[u8; 335] = b"This is a longer message and its length is over two SHA-384 blocks long, where each block is 128 bytes. This allows us to test the case where multiple calls to compress are required during the update phase. Depending on how much data is already in the buffer, calling update() on this should result in either two or three compressions.";
+
 /// This function isn't particularly interesting. It only exists to make
 /// `cargo saw-build` keep the relevant bits of `sha2`'s API in the generated
 /// MIR JSON file.
 pub fn example() {
-    Sha384::digest(b"hello world");
+    Sha384::digest(HELLO);
+    Sha384::digest(LONG_MESSAGE);
 
     let mut hasher = Sha384::new();
-    hasher.update(b"hello world");
+    hasher.update(HELLO);
+    hasher.update(LONG_MESSAGE);
     hasher.finalize();
 }
