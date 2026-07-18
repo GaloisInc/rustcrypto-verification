d5cb02e5e7cab476746cc5d16d6fc6f2cd4bb7cf
Bretton <qsctr@users.noreply.github.com>
2026-07-18 01:47:21 -0700

    aes: Verify aes{128,192,256}_{en,de}crypt directly

--- aes-verif/src/lib.rs
+++ aes-verif/src/lib.rs
@@ -6,55 +6,55 @@ use aes::{
 };
 
 /// Encrypt a block using AES-128.
-pub fn encrypt128(key: [u8; 16], block: [u8; 16]) -> [u8; 16] {
+pub fn toplevel_encrypt_block_128(key: [u8; 16], block: [u8; 16]) -> [u8; 16] {
     let key = GenericArray::from(key);
-    let mut block = GenericArray::from(block);
     let cipher = Aes128::new(&key);
+    let mut block = GenericArray::from(block);
     cipher.encrypt_block(&mut block);
     block.into()
 }
 
 /// Decrypt a block using AES-128.
-pub fn decrypt128(key: [u8; 16], block: [u8; 16]) -> [u8; 16] {
+pub fn toplevel_decrypt_block_128(key: [u8; 16], block: [u8; 16]) -> [u8; 16] {
     let key = GenericArray::from(key);
-    let mut block = GenericArray::from(block);
     let cipher = Aes128::new(&key);
+    let mut block = GenericArray::from(block);
     cipher.decrypt_block(&mut block);
     block.into()
 }
 
 /// Encrypt a block using AES-192.
-pub fn encrypt192(key: [u8; 24], block: [u8; 16]) -> [u8; 16] {
+pub fn toplevel_encrypt_block_192(key: [u8; 24], block: [u8; 16]) -> [u8; 16] {
     let key = GenericArray::from(key);
-    let mut block = GenericArray::from(block);
     let cipher = Aes192::new(&key);
+    let mut block = GenericArray::from(block);
     cipher.encrypt_block(&mut block);
     block.into()
 }
 
 /// Decrypt a block using AES-192.
-pub fn decrypt192(key: [u8; 24], block: [u8; 16]) -> [u8; 16] {
+pub fn toplevel_decrypt_block_192(key: [u8; 24], block: [u8; 16]) -> [u8; 16] {
     let key = GenericArray::from(key);
-    let mut block = GenericArray::from(block);
     let cipher = Aes192::new(&key);
+    let mut block = GenericArray::from(block);
     cipher.decrypt_block(&mut block);
     block.into()
 }
 
 /// Encrypt a block using AES-256.
-pub fn encrypt256(key: [u8; 32], block: [u8; 16]) -> [u8; 16] {
+pub fn toplevel_encrypt_block_256(key: [u8; 32], block: [u8; 16]) -> [u8; 16] {
     let key = GenericArray::from(key);
-    let mut block = GenericArray::from(block);
     let cipher = Aes256::new(&key);
+    let mut block = GenericArray::from(block);
     cipher.encrypt_block(&mut block);
     block.into()
 }
 
 /// Decrypt a block using AES-256.
-pub fn decrypt256(key: [u8; 32], block: [u8; 16]) -> [u8; 16] {
+pub fn toplevel_decrypt_block_256(key: [u8; 32], block: [u8; 16]) -> [u8; 16] {
     let key = GenericArray::from(key);
-    let mut block = GenericArray::from(block);
     let cipher = Aes256::new(&key);
+    let mut block = GenericArray::from(block);
     cipher.decrypt_block(&mut block);
     block.into()
 }
